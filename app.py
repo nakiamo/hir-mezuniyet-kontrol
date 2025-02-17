5cc304e8e86df5e30642d9f8ea485aac8f9c395a
naim cinar <naimcinar@anadolu.edu.tr>
2025-02-17 11:27:06 +0300

    changed python code-39

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from pdf2image import convert_from_path
 import pytesseract
 
-images = convert_from_path("test.pdf", poppler_path="/usr/bin/poppler-utils")
+images = convert_from_path(uploaded_file, poppler_path="/usr/bin/")
 for img in images:
     text = pytesseract.image_to_string(img)
     st.write("OCR Çıktısı:", text)
